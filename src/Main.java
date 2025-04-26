4053110536d004b3ed2223cf1ef99b989364ca7b
heartshrimal <155750703+heartshrimal@users.noreply.github.com>
2025-04-26 18:09:19 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
 import gui.LoginUI;
-import utils.DeviceInitializer;
-import devices.Device;
+import users.Admin;
+import users.RegularUser;
+import users.User;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         // Initialize the shared list of devices
-        List<Device> devices = DeviceInitializer.initializeDevices();
+        // List<Device> devices = DeviceInitializer.initializeDevices();
+        List<User> users = new ArrayList<>();
 
+        users.add(new Admin("admin","admin123"));
+        users.add(new RegularUser("user1", "password1"));
+        users.add(new RegularUser("user2", "password2"));
+        
         // Pass the devices list to the LoginUI
-        LoginUI loginUI = new LoginUI(devices);
+        LoginUI loginUI = new LoginUI(users);
         loginUI.setVisible(true);
     }
 }
