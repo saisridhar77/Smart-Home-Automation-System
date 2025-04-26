148c05dd8f631fc42c9a6a5a0fa3dc79fcdb156c
heartshrimal <155750703+heartshrimal@users.noreply.github.com>
2025-04-26 18:13:32 +0530

    Update User.java

--- src/users/User.java
+++ src/users/User.java
@@ -1,12 +1,18 @@
 // User.java (Abstract Class)
 package users;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import devices.Device;
 import enums.UserRole;
 
 public abstract class User {
     private String username;
     private String password;
     private UserRole role;
+    private List<Device> devices = new ArrayList<>();
+
 
     public User(String username, String password, UserRole role) {
         this.username = username;
@@ -26,4 +32,20 @@ public abstract class User {
     public String getUsername() { return username; }
     public UserRole getRole() { return role; }
     public boolean checkPassword(String password) { return this.password.equals(password); }
+
+    public void addDevices(Device... devices) {
+        for (Device device : devices) {
+            this.devices.add(device);
+        }
+    }
+    
+    public List<Device> getDevices() {
+        return devices;
+    }
+    
+    public void removeDevice(Device device) {
+        devices.remove(device);
+    }
+    
+    
 }
